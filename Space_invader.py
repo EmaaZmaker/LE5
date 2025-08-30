d0fb64311fe311b8cddf3d17e18a64c61cf8506a
EmaaZmaker <emaazbinammar@gmail.com>
2025-08-30 10:56:38 +0400

    ok

--- Space_invader.py
+++ Space_invader.py
@@ -96,3 +96,53 @@ def isCollision(enemy_x, enemy_y, bullet_x, bullet_y):
     distance = math.sqrt((enemy_x - bullet_x) ** 2 + (enemy_y - bullet_y) ** 2)
 
     return distance < collsion_distance
+running=True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+            if event.type==pygame.K_LEFT:
+                player_x_change=-5
+            if event.type==pygame.K_RIGHT:
+                player_x_change=5
+            if event.key==pygame.K_SPACE and bullet_state=="ready":
+                bullet_x=player_x
+                fire_bullet(bullet_x,bullet_y)
+        if event.type==pygame.KEYUP and event.key in [pygame.K_LEFT,pygame.K_RIGHT]:
+            player_x_change=0
+    player_x=player_x+player_x_change
+    player_x=max(0,min(player_x,screen_width-64))
+    
+    #enemy movement:
+    for i in range(number_of_enemies):
+        if enemy_y[i]>340:
+            for j in range(number_of_enemies):
+                enemy_y[j]=2000
+            game_over_text()
+            break
+        enemy_x[i]+=enemy_x_change[i]
+        if enemy_x[i]<=0 or enemy_x[i]>=screen_width-64:
+            enemy_x_change[i]*=-1
+            enemy_y[i]+=enemy_y_change[i]
+        
+        #collision check
+
+        if isCollision(enemy_x[i],enemy_y[i],bullet_x,bullet_y):
+            bullet_y=player_start_y
+            bullet_state="ready"
+            score_value=score_value+1
+            enemy_x[i]=random.randint(0,screen_width-64)
+            enemy_y[i]=random.randint(enemy_start_y_min,enemy_start_y_max)
+        enemy(enemy_x[i],enemy_y[i],i)
+    if bullet_y<=0:
+        bullet_y=player_start_y
+        bullet_state="ready"
+    elif bullet_state=="fire":
+        fire_bullet(bullet_x,bullet_y)
+        bullet_y=bullet_y-bullet_y_change
+    player(player_x,player_y)
+    show_score(text_x,text_y)
+    pygame.display.update()
